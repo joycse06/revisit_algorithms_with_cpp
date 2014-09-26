b217fce860362562db606b48697eb12e182bb52e
Joy Nag <joyruet06@gmail.com>
2014-09-26 03:19:36 -0400

    Initial commit

---
+++ sandbox/start.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	int joy;
+	cout << "Enter a Number: " << endl;
+	cin >> joy;
+
+	cout << "I am the first program. : " << joy;
+
+	return 0;
+}
